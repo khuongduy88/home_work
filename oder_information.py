7bdbbe2bbfb8be77b76fad3d0ad162d25ef69c94
khuongduy88 <duycdt1k51@gmail.com>
2022-10-25 23:01:44 +0900

    oder information

---
+++ oder_information.py
@@ -0,0 +1,10 @@
+num = float(input("Give me a price: "))
+
+if num>=150:
+    print("Total: ",num-50)
+elif num>=100:
+    print("Total: ",num-25)
+elif num>=75:
+    print("Total: ",num -15)
+else:
+    print("Total: ",num)
